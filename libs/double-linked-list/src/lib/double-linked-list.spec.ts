e1a2d84a80609b60a15d96dbaf0fefd881350440
Alessandro Maclaine <almaclaine@gmail.com>
2023-02-06 00:16:42 -0600

    add more features

--- libs/double-linked-list/src/lib/double-linked-list.spec.ts
+++ libs/double-linked-list/src/lib/double-linked-list.spec.ts
@@ -5,7 +5,6 @@ describe('DoubleLinkedList', () => {
   it('list should start out with head/tail undefined', () => {
     const dll = new DoubleLinkedList();
     expect(dll.peekFirst()).toEqual(undefined);
-    expect(dll.peek()).toEqual(undefined);
     expect(dll.peekLast()).toEqual(undefined);
   });
 
@@ -13,8 +12,7 @@ describe('DoubleLinkedList', () => {
     const dll = new DoubleLinkedList<number>();
     dll.addFront(2);
     expect(dll.peekFirst()).toMatchObject({ value: 2 });
-    expect(dll.peek()).toEqual(dll.peekFirst());
-    expect(dll.peek()).toEqual(dll.peekLast());
+    expect(dll.peekFirst()).toEqual(dll.peekFirst());
   });
 
   it('addFront 2 node should be connected', () => {
@@ -51,8 +49,7 @@ describe('DoubleLinkedList', () => {
     const dll = new DoubleLinkedList<number>();
     dll.addBack(2);
     expect(dll.peekFirst()).toMatchObject({ value: 2 });
-    expect(dll.peek()).toEqual(dll.peekFirst());
-    expect(dll.peek()).toEqual(dll.peekLast());
+    expect(dll.peekFirst()).toEqual(dll.peekFirst());
   });
 
   it('addBack 3 node should be correct', () => {
@@ -215,5 +212,141 @@ describe('DoubleLinkedList', () => {
     dll.addNodeAfterNode(node4, node1);
     expect(dll.peekFirst()?.nextNode).toEqual(node4);
     expect(dll.peekLast()?.prevNode?.prevNode).toEqual(node4);
+
+    expect(dll.peekFirst()).toMatchObject({ value: 2 });
+    expect(dll.peekLast()).toMatchObject({ value: 3 });
+  });
+
+  it('clear works', () => {
+    const dll = new DoubleLinkedList<number>();
+    const node1 = new Node<number>({ value: 2 });
+    dll.addNodeFront(node1);
+
+    const node2 = new Node<number>({ value: 3 });
+    dll.addNodeAfterNode(node2, node1);
+
+    const node3 = new Node<number>({ value: 4 });
+    dll.addNodeAfterNode(node3, node1);
+
+    const node4 = new Node<number>({ value: 5 });
+    dll.addNodeAfterNode(node4, node1);
+    expect(dll.peekFirst()?.nextNode).toEqual(node4);
+    expect(dll.peekLast()?.prevNode?.prevNode).toEqual(node4);
+
+    dll.clear();
+    expect(dll.peekFirst()).toEqual(undefined);
+    expect(dll.peekLast()).toEqual(undefined);
+  });
+
+  it('clone works', () => {
+    const dll = new DoubleLinkedList<number>();
+    const node1 = new Node<number>({ value: 2 });
+    dll.addNodeFront(node1);
+    const node2 = new Node<number>({ value: 3 });
+    dll.addNodeAfterNode(node2, node1);
+    expect(dll.peekFirst()).toMatchObject({ value: 2 });
+    expect(dll.peekLast()).toMatchObject({ value: 3 });
+    expect(dll.peekFirst() === dll.peekFirst()).toEqual(true);
+
+    const node3 = new Node<number>({ value: 4 });
+    dll.addNodeAfterNode(node3, node1);
+    expect(dll.peekFirst()?.nextNode).toEqual(node3);
+    expect(dll.peekLast()?.prevNode).toEqual(node3);
+
+    const node4 = new Node<number>({ value: 5 });
+    dll.addNodeAfterNode(node4, node1);
+    expect(dll.peekFirst()?.nextNode).toEqual(node4);
+    expect(dll.peekLast()?.prevNode?.prevNode).toEqual(node4);
+
+    const dll2 = dll.clone();
+
+    expect(dll2.peekFirst()).toMatchObject({ value: 2 });
+    expect(dll2.peekLast()).toMatchObject({ value: 3 });
+
+    expect(dll2.peekFirst() === dll.peekFirst()).toEqual(false);
+    expect(dll2.peekLast() === dll.peekLast()).toEqual(false);
+  });
+
+  it('contains node works', () => {
+    const dll = new DoubleLinkedList<number>();
+    const node1 = new Node<number>({ value: 2 });
+    dll.addNodeFront(node1);
+    expect(dll.containsNode(node1)).toBeTruthy();
+  });
+
+  it('contains value works', () => {
+    const dll = new DoubleLinkedList<number>();
+    const node1 = new Node<number>({ value: 2 });
+    dll.addNodeFront(node1);
+    expect(dll.containsValue(2)).toBeTruthy();
+
+    const value = { a: 2 };
+    const dll2 = new DoubleLinkedList<typeof value>();
+    const node2 = new Node<typeof value>({ value });
+    dll2.addNodeFront(node2);
+    expect(dll2.containsValue(value)).toBeTruthy();
+  });
+
+  it('iterator works', () => {
+    const dll = new DoubleLinkedList<number>();
+    const values = [2, 3, 4, 5];
+    const node1 = new Node<number>({ value: values[0] });
+    dll.addNodeFront(node1);
+    const node2 = new Node<number>({ value: values[1] });
+    dll.addNodeAfterNode(node2, node1);
+    const node3 = new Node<number>({ value: values[2] });
+    dll.addNodeAfterNode(node3, node1);
+    const node4 = new Node<number>({ value: values[3] });
+    dll.addNodeAfterNode(node4, node1);
+
+    let i = 0;
+    const nodes = [node1, node4, node3, node2];
+    for (const node of dll) {
+      expect(node).toEqual(nodes[i++].value);
+    }
+  });
+
+  it('length works', () => {
+    const dll = new DoubleLinkedList<number>();
+    const values = [2, 3, 4, 5];
+    const node1 = new Node<number>({ value: values[0] });
+    dll.addNodeFront(node1);
+    const node2 = new Node<number>({ value: values[1] });
+    dll.addNodeAfterNode(node2, node1);
+    const node3 = new Node<number>({ value: values[2] });
+    dll.addNodeAfterNode(node3, node1);
+    const node4 = new Node<number>({ value: values[3] });
+    dll.addNodeAfterNode(node4, node1);
+
+    expect(dll.length).toEqual(4);
+  });
+
+  it('add node index works', () => {
+    const dll = new DoubleLinkedList<number>();
+    const node1 = new Node<number>({ value: 2 });
+    dll.addNodeIndex(node1, -1);
+    expect(dll.peekFirst()).toEqual(node1);
+
+    const node2 = new Node<number>({ value: 3 });
+    dll.addNodeIndex(node2, 10);
+    expect(dll.peekLast()).toEqual(node2);
+
+    const node3 = new Node<number>({ value: 4 });
+    dll.addNodeIndex(node3, 1);
+    expect(dll.peekFirst()?.nextNode).toEqual(node3);
+    expect(dll.peekLast()?.prevNode).toEqual(node3);
+  });
+
+  it('add index works', () => {
+    const dll = new DoubleLinkedList<number>();
+    dll.addIndex(2, -1);
+    expect(dll.peekFirst()?.value).toEqual(2);
+
+    dll.addIndex(3, 10);
+    expect(dll.peekLast()?.value).toEqual(3);
+
+    dll.addIndex(4, 1);
+    expect(dll.peekFirst()?.nextNode?.value).toEqual(4);
+    expect(dll.peekLast()?.prevNode?.value).toEqual(4);
   });
 });
--- libs/double-linked-list/src/lib/double-linked-list.ts
+++ libs/double-linked-list/src/lib/double-linked-list.ts
@@ -15,7 +15,7 @@ class Node<T> {
     this._value = value;
   }
 
-  get value() {
+  get value(): T {
     return this._value;
   }
 }
@@ -24,32 +24,60 @@ class DoubleLinkedList<T> {
   private tail?: Node<T>;
   private head?: Node<T>;
   private nodeSet = new Set<Node<T>>();
+  private valueSet = new Set<T>();
 
-  addFront(value: T) {
+  addFront(value: T): void {
     const newNode = new Node<T>({ value });
     this.addNodeFront(newNode);
   }
 
-  addFrontMany(values: T[]) {
+  addFrontMany(values: T[]): void {
     values.forEach((val) => this.addFront(val));
   }
 
-  addBack(value: T) {
+  addBack(value: T): void {
     const newNode = new Node<T>({ value });
     this.addNodeBack(newNode);
   }
 
-  addBackMany(values: T[]) {
+  addIndex(value: T, index: number): void {
+    const newNode = new Node<T>({ value });
+    this.addNodeIndex(newNode, index);
+  }
+
+  addBackMany(values: T[]): void {
     values.forEach((val) => this.addBack(val));
   }
 
-  addNodeFront(node: Node<T>) {
-    if (this.nodeSet.has(node)) {
+  private addNode(node: Node<T>) {
+    this.valueSet.add(node.value);
+    if (this.containsNode(node)) {
       throw new Error('Cannot add same node multiple times');
     } else {
       this.nodeSet.add(node);
     }
-    this.nodeSet.add(node);
+  }
+
+  addNodeIndex(node: Node<T>, index: number) {
+    if (index <= 0) {
+      this.addNodeFront(node);
+    } else if (index >= this.length) {
+      this.addNodeBack(node);
+    } else {
+      let tmpNode = this.head;
+      let i = 0;
+      while (tmpNode) {
+        if (i++ === index - 1) {
+          this.addNodeAfterNode(node, tmpNode);
+          return;
+        }
+        tmpNode = tmpNode.nextNode;
+      }
+    }
+  }
+
+  addNodeFront(node: Node<T>): void {
+    this.addNode(node);
     if (!this.head && !this.tail) {
       this.addNodeEmpty(node);
     } else if (this.head && this.head === this.tail) {
@@ -58,7 +86,6 @@ class DoubleLinkedList<T> {
       this.tail.prevNode = this.head;
     } else {
       if (this.head) {
-        this.addNodeBeforeNode(node, this.head);
         node.nextNode = this.head;
         this.head.prevNode = node;
         this.head = node;
@@ -68,20 +95,15 @@ class DoubleLinkedList<T> {
     }
   }
 
-  addNodeFrontMany(nodes: Node<T>[]) {
+  addNodeFrontMany(nodes: Node<T>[]): void {
     nodes.forEach((node) => this.addNodeFront(node));
   }
 
-  addNodeBack(node: Node<T>) {
-    if (this.nodeSet.has(node)) {
-      throw new Error('Cannot add same node multiple times');
-    } else {
-      this.nodeSet.add(node);
-    }
-
+  addNodeBack(node: Node<T>): void {
+    this.addNode(node);
     if (!this.head && !this.tail) {
       this.addNodeEmpty(node);
-    } else if (this.head && this.head === this.tail) {
+    } else if (this.tail && this.head === this.tail) {
       this.tail = node;
       this.head.nextNode = this.tail;
       this.tail.prevNode = this.head;
@@ -96,11 +118,11 @@ class DoubleLinkedList<T> {
     }
   }
 
-  addNodeBackMany(nodes: Node<T>[]) {
+  addNodeBackMany(nodes: Node<T>[]): void {
     nodes.forEach((node) => this.addNodeBack(node));
   }
 
-  addNodeBeforeNode(nodeToAdd: Node<T>, beforeNode: Node<T>) {
+  addNodeBeforeNode(nodeToAdd: Node<T>, beforeNode: Node<T>): void {
     if (!this.nodeSet.has(beforeNode)) {
       throw new Error('Before node is not part of list');
     }
@@ -123,7 +145,7 @@ class DoubleLinkedList<T> {
     }
   }
 
-  addNodeAfterNode(nodeToAdd: Node<T>, afterNode: Node<T>) {
+  addNodeAfterNode(nodeToAdd: Node<T>, afterNode: Node<T>): void {
     if (!this.nodeSet.has(afterNode)) {
       throw new Error('Before node is not part of list');
     }
@@ -146,21 +168,56 @@ class DoubleLinkedList<T> {
     }
   }
 
-  private addNodeEmpty(node: Node<T>) {
+  private addNodeEmpty(node: Node<T>): void {
     this.head = node;
     this.tail = this.head;
   }
 
-  peek() {
-    return this.peekFirst();
-  }
-
-  peekFirst() {
+  peekFirst(): Node<T> | undefined {
     return this?.head;
   }
 
-  peekLast() {
+  peekLast(): Node<T> | undefined {
     return this?.tail;
+  }
+
+  clear() {
+    this.head = undefined;
+    this.tail = undefined;
+  }
+
+  containsNode(node: Node<T>) {
+    return this.nodeSet.has(node);
+  }
+
+  containsValue(value: T) {
+    return this.valueSet.has(value);
+  }
+
+  clone(): DoubleLinkedList<T> {
+    const dll = new DoubleLinkedList<T>();
+    let node = this.head;
+    while (node !== undefined) {
+      dll.addBack(node.value);
+      node = node.nextNode;
+    }
+    return dll;
+  }
+
+  get length() {
+    return this.nodeSet.size;
+  }
+
+  [Symbol.iterator]() {
+    let tmpNode = this.head;
+
+    return {
+      next: () => {
+        const out = { value: tmpNode?.value, done: !tmpNode };
+        tmpNode = tmpNode?.nextNode;
+        return out;
+      },
+    };
   }
 }
 
