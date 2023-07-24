9b6606e89d31794ee57bcbc436a5f1b266220e90
Alessandro Maclaine <almaclaine@gmail.com>
2023-07-23 19:58:01 -0500

    v0.0.78

--- libs/cdk/src/lib/stacks/s3-stacks.ts
+++ libs/cdk/src/lib/stacks/s3-stacks.ts
@@ -1,18 +1,17 @@
 import type { Stack } from '../stack';
 import { merge } from 'moderndash';
-import type { CfnS3BucketName } from '../utils/cfn-outputs/cfn-outputs-s3';
 import type { S3ConstructOptions } from '../constructs/s3.construct';
 import { S3Construct } from '../constructs/s3.construct';
 
 type CreateS3StackOptions<
   StackName extends string,
-  BucketName extends CfnS3BucketName
+  BucketName extends string
 > = Omit<S3ConstructOptions<StackName, BucketName>, 'stackName'>;
 
-function createS3Stack<
-  StackName extends string,
-  BucketName extends CfnS3BucketName
->(stack: Stack, props: CreateS3StackOptions<StackName, BucketName>) {
+function createS3Stack<StackName extends string, BucketName extends string>(
+  stack: Stack,
+  props: CreateS3StackOptions<StackName, BucketName>
+) {
   S3Construct.of(
     stack.stack,
     merge(props, {
