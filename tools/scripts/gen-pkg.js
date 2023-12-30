f11bee618aae3e22ece3c3091de9785c7577f252
Alessandro Maclaine <almaclaine@gmail.com>
2023-12-30 17:51:20 -0600

    add gen-pkg script and linkwarden stub

---
+++ tools/scripts/gen-pkg.js
@@ -0,0 +1,13 @@
+const {exec} = require('child_process');
+
+// Grab provided arguments
+const [packageName] = process.argv.slice(2);
+
+// Construct the command
+const command = `pnpm nx g @nrwl/js:lib ${packageName} --publishable --importPath=@almaclaine/${packageName} --buildable --compiler=swc`;
+
+// Execute the command
+exec(command, (error, stdout, stderr) => {
+  // Error checks omitted for brevity
+  console.log(stdout);
+});
